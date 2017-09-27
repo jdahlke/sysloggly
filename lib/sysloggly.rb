14dc83a3eab4a732a46d36a0762cf4d6e84c7a25
Joergen Dahlke <joergen.dahlke@infopark.de>
2017-09-27 15:47:32 +0200

    put changes together

--- lib/sysloggly.rb
+++ lib/sysloggly.rb
@@ -1,5 +1,12 @@
-require 'logger'
-require 'lograge'
+require "logger"
+require "lograge"
+require "uri"
+
+require "sysloggly/version"
+require "sysloggly/clients/filelog"
+require "sysloggly/clients/networklog"
+require "sysloggly/formatters/simple_formatter"
+require "sysloggly/formatters/syslog_formatter"
 
 #
 # Sysloggly
@@ -16,22 +23,38 @@ module Sysloggly
     yield self
   end
 
-  def self.new(url, opts = {})
-    client = Sysloggly::Client.new(url, opts)
-    logger = Logger.new(client)
 
-    if client.respond_to?(:formatter)
-      logger.formatter = client.formatter
-    elsif client.respond_to?(:datetime_format)
-      logger.datetime_format = client.datetime_format
+  # Creates a new logger instance
+  #
+  # @return [Logger]
+  # @api public
+  def self.new(url, opts = {})
+    raise InputURLRequired.new  unless url
+
+    begin
+      input_uri = URI.parse(url)
+    rescue URI::InvalidURIError => e
+      raise InputURLRequired.new("Invalid Input URL: #{url}")
     end
+
+    client, formatter = nil
+
+    case input_uri.scheme
+    when "file"
+      client = Sysloggly::Clients::Filelog.new(input_uri.path)
+      formatter = Sysloggly::Formatters::SimpleFormatter.new(input_uri, opts)
+    when "udp", "tcp"
+      client = Sysloggly::Clients::Networklog.new(input_uri, opts)
+      formatter = Sysloggly::Formatters::SyslogFormatter.new(input_uri, opts)
+    else
+      raise Sysloggly::UnsupportedScheme.new("#{input_uri.scheme} is unsupported")
+    end
+
+    logger = Logger.new(client)
+    logger.formatter = formatter
 
     logger
   end
 end
 
-require 'sysloggly/version'
-require 'sysloggly/client'
-require 'sysloggly/client/filelog'
-require 'sysloggly/client/syslog'
-require 'sysloggly/rails'  if Object.const_defined?(:Rails) && Rails.const_defined?(:Railtie)
+require "sysloggly/rails"  if Object.const_defined?(:Rails) && Rails.const_defined?(:Railtie)
